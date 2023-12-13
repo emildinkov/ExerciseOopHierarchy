bb2fe44a02f4279034eda870fff506b7087c2d78
emildinkov <135504796+emildinkov@users.noreply.github.com>
2023-12-13 13:47:56 +0200

    Add project files.

---
+++ Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseOopHierarchy
+{
+    public class Order
+    {
+        private List<MenuItem> _items = new(); // Правим списък да можем да съхраняваме поръчки.
+
+        public IReadOnlyCollection<MenuItem> Items => this._items.AsReadOnly(); // По този начин виждаме всички елементи от поръчката но не можем нищо друго да правим с тях.
+
+        public void AddItem(MenuItem item)      // Метод за добавяне на поръчки.
+        {
+            this._items.Add(item);
+        }
+
+        public decimal GetTotal()   // Правим метод който сумира всички поръчки в списъка.
+        {
+            return this._items.Select(x => x.Price).Sum();
+        }
+    }
+}
